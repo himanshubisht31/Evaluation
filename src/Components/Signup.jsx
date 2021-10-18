9a5513706e5276354193854b4ae3a077f38b4a25
Himanshu <himanshubisht3112@gmail.com>
2021-10-18 14:39:14 +0530

    1st

---
+++ src/Components/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <div>
+
+            <form action="">
+
+                <input type="text" placeholder='name' />
+                <input type="number" placeholder='age'/>
+                <input type="text" placeholder='location'/>
+                <input type="checkbox" placeholder='Topics of interest'/>
+
+            </form>
+
+
+            
+        </div>
+    )
+}
